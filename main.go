1e22d8c4499bd8c68fad9a229f46f6b22abcfaed
Aditya <aditya999123@gmail.com>
2019-03-25 13:18:58 +0530

    pow added

--- main.go
+++ main.go
@@ -3,18 +3,23 @@ package main
 import (
     "fmt"
     "github.com/aditya999123/BlockChain-101/blockchain"
+    "strconv"
 )
 
 func main() {
     chain := blockchain.InitBlockChain()
 
-    chain.AddBlock("First block after genisis")
-    chain.AddBlock("Second block after genisis")
-    chain.AddBlock("Third block after genisis")
+    chain.AddBlock("First Block after Genesis")
+    chain.AddBlock("Second Block after Genesis")
+    chain.AddBlock("Third Block after Genesis")
 
     for _, block := range chain.Blocks {
         // fmt.Printf("Previous Hash: %x\n", block.PrevHash)
         fmt.Printf("Data in block: %s\n", block.Data)
         fmt.Printf("Hash: %x\n", block.Hash)
+
+        pow := blockchain.NewProof(block)
+        fmt.Printf("Pow: %s\n", strconv.FormatBool(pow.Validate()))
+        fmt.Println()
     }
 }
