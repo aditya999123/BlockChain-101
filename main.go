3dac783e2a35b6306fb06f01845469f8410b3b69
Aditya <aditya999123@gmail.com>
2019-03-25 11:34:28 +0530

    restructuring

--- main.go
+++ main.go
@@ -2,54 +2,17 @@ package main
 
 import (
     "fmt"
-    "bytes"
-    "crypto/sha256"
+    "github.com/aditya999123/BlockChain-101/blockchain"
 )
 
-type Block struct {
-    Hash     []byte
-    Data     []byte
-    PrevHash []byte 
-}
-
-func (b *Block) DeriveHash() {
-    info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
-    hash := sha256.Sum256(info)
-    b.Hash = hash[:]
-}
-
-func CreateBlock(data string, prevHash []byte) *Block {
-    block := &Block{[]byte{}, []byte(data), prevHash}
-    block.DeriveHash()
-    return block
-}
-
-type BlockChain struct {
-    blocks []*Block
-}
-
-func (chain *BlockChain) AddBlock (data string) {
-    prevBlock := chain.blocks[len(chain.blocks)-1]
-    newBlock := CreateBlock(data, prevBlock.Hash)
-    chain.blocks = append(chain.blocks, newBlock)
-}
-
-func Genesis() *Block {
-    return CreateBlock("Genesis", []byte{})
-}
-
-func InitBlockChain() *BlockChain {
-    return &BlockChain{[]*Block{Genesis()}}
-}
-
 func main() {
-    chain := InitBlockChain()
+    chain := blockchain.InitBlockChain()
 
     chain.AddBlock("First block after genisis")
     chain.AddBlock("Second block after genisis")
     chain.AddBlock("Third block after genisis")
 
-    for _, block := range chain.blocks {
+    for _, block := range chain.Blocks {
         // fmt.Printf("Previous Hash: %x\n", block.PrevHash)
         fmt.Printf("Data in block: %s\n", block.Data)
         fmt.Printf("Hash: %x\n", block.Hash)
